c0cbe2f957ed99aa6c8497e0f764e74bf619bd45
Chris Shepherd <chris@loneless.org>
2018-01-11 00:09:52 +0100

    Manage roles on other users

--- features/role.js
+++ features/role.js
@@ -1,4 +1,5 @@
 const log = require("../modules/log");
+const Discord = require("discord.js");
 const Command = require("../modules/Command");
 
 const available_roles = {
@@ -27,71 +28,139 @@ for (let category in available_roles)
         roles_array[role.toLowerCase()] = role;
 
 const command = new Command(async function onmessage(message) {
-    let text = message.content.toLowerCase();
-    if (text.startsWith("!selfrole remove")) {
-        text = text.trim().split(/ +/g).join(" "); // remove double spaces
-        let role = text.replace("!selfrole remove", "");
+    if (/^!role remove\b/.test(message.content)) {
+        const role = message.content.trim().split(/ +/g).join(" ").substr(13);
         if (role === "") {
             message.channel.send(this.usage);
             return;
         }
-        role = role.substring(1);
 
-        if (!roles_array[role]) {
-            message.channel.send("Hmm... I couldn't really find your role. Check that again");
+        const members = message.mentions.members.array();
+        const permission = message.channel.permissionsFor(message.member).has(Discord.Permissions.FLAGS.MANAGE_ROLES);
+        if (!permission) {
+            message.channel.send("IDK what you're doing here, Mister Not-Allowed-To-Remove-Role-From-Somebody-Else. To use the role command you must have permissions to manage roles.");
             return;
         }
+        if (members.length > 0) {
 
-        const role_obj = message.guild.roles.find("name", roles_array[role]);
-        if (!role_obj) {
-            message.channel.send("Uh apparently this server doesn't have this role available right now.");
-            return;
+            let role2 = role;
+
+            for (const member of members) {
+                if (message.channel.permissionsFor(member).has(Discord.Permissions.FLAGS.MANAGE_ROLES)) {
+                    message.channel.send("You cannot add roles to other users with Manage Roles permission.");
+                    return;
+                }
+                role2 = role2.replace(member.toString(), "");
+            }
+            role2 = role2.trim();
+
+            const role_obj = message.guild.roles.find("name", role2);
+            if (!role_obj) {
+                message.channel.send("Uh apparently this server doesn't have this role available right now.");
+                return;
+            }
+
+            for (const member of members) {
+                member.removeRole(role_obj);
+            }
+            message.channel.send("Role removed.");
+            log(`Removed role ${roles_array[role]} from users ${members.map(member => member.toString()).join(" ")}`);
+        } else {
+            const role2 = permission ? role : roles_array[role.toLowerCase()];
+            if (!role2) {
+                message.channel.send("Hmm... I couldn't really find your role. Check that again");
+                return;
+            }
+
+            const role_obj = message.guild.roles.find("name", role2);
+            if (!role_obj) {
+                message.channel.send("Uh apparently this server doesn't have this role available right now.");
+                return;
+            }
+
+            if (!message.member.roles.has(role_obj.id)) {
+                message.channel.send("Can't remove a role without having it first.");
+                return;
+            }
+
+            await message.member.removeRole(role_obj);
+            message.channel.send("Role removed.");
+            log(`Removed role ${role2} from user ${message.member.user.username}`);
         }
-
-        if (!message.member.roles.has(role_obj.id)) {
-            message.channel.send("Can't remove a role without having it first.");
-            return;
-        }
-
-        await message.member.removeRole(role_obj);
-        message.channel.send("Role removed.");
-        log(`Removed role ${roles_array[role]} from user ${message.member.user.username}`);
     }
-    else if (text.startsWith("!selfrole")) {
-        text = text.trim().split(/ +/g).join(" "); // remove double spaces
-        let role = text.replace("!selfrole", "");
+    else if (/^!role\b/.test(message.content)) {
+        const role = message.content.trim().split(/ +/g).join(" ").substr(6);
         if (role === "") {
             message.channel.send(this.usage);
             return;
         }
-        role = role.substring(1);
 
-        if (!roles_array[role]) {
-            message.channel.send("Hmm... I couldn't really find your role. Here's a list of available ones:\n" + roles_message);
+        const members = message.mentions.members.array();
+        const permission = message.channel.permissionsFor(message.member).has(Discord.Permissions.FLAGS.MANAGE_ROLES);
+        if (!permission) {
+            message.channel.send("IDK what you're doing here, Mister Not-Allowed-To-Add-Role-To-Somebody-Else. To use the role command you must have permissions to manage roles.");
             return;
         }
+        if (members.length > 0) {
+            const permission = message.channel.permissionsFor(message.member).has(Discord.Permissions.FLAGS.MANAGE_ROLES);
+            if (!permission) {
+                message.channel.send("IDK what you're doing here, Mister Not-Allowed-To-Add-Role-To-Somebody-Else. To use the role command you must have permissions to manage roles.");
+                return;
+            }
 
-        const role_obj = message.guild.roles.find("name", roles_array[role]);
-        if (!role_obj) {
-            message.channel.send("Uh apparently this server doesn't have this role available right now.");
-            return;
+            let role2 = role;
+
+            for (const member of members) {
+                if (message.channel.permissionsFor(member).has(Discord.Permissions.FLAGS.MANAGE_ROLES)) {
+                    message.channel.send("You cannot add roles to other users with Manage Roles permission.");
+                    return;
+                }
+                role2 = role2.replace(member.toString(), "");
+            }
+            role2 = role2.trim();
+
+            const role_obj = message.guild.roles.find("name", role2);
+            if (!role_obj) {
+                message.channel.send("Uh apparently this server doesn't have this role available right now.");
+                return;
+            }
+
+            for (const member of members) {
+                await member.addRole(role_obj);
+            }
+            message.channel.send("Role added! /)");
+            log(`Added role ${roles_array[role]} to users ${members.map(member => member.toString()).join(" ")}`);
+        } else {
+            const role2 = permission ? role : roles_array[role.toLowerCase()];
+            if (!role2) {
+                message.channel.send("Hmm... I couldn't really find your role. Here's a list of available ones:\n" + roles_message);
+                return;
+            }
+
+            const role_obj = message.guild.roles.find("name", role2);
+            if (!role_obj) {
+                message.channel.send("Uh apparently this server doesn't have this role available right now.");
+                return;
+            }
+
+            if (message.member.roles.has(role_obj.id)) {
+                message.channel.send("You already have this role! Yay?");
+                return;
+            }
+
+            await message.member.addRole(role_obj);
+            message.channel.send("Role added! /)");
+            log(`Added role ${role2} to user ${message.member.displayName}`);
         }
-
-        if (message.member.roles.has(role_obj.id)) {
-            message.channel.send("You already have this role! Yay?");
-            return;
-        }
-
-        await message.member.addRole(role_obj);
-        message.channel.send("Role added! /)");
-        log(`Added role ${roles_array[role]} to user ${message.member.user.username}`);
     }
 }, {
-    usage: `\`!selfrole <role>\` to add
+    usage: `\`!role <role> <?user mention 1> <?user mention 2> ...\` to add
 \`role\` - The role you would like to have added
+\`user mention\` - this is irrelevant to you, if you don't have rights to manage roles yourself.
 
-\`!selfrole remove <role>\` to remove
-\`role\` - The role you would like to have removed`,
+\`!role remove <role> <?user mention 1> <?user mention 2> ...\` to remove
+\`role\` - The role you would like to have removed
+\`user mention\` - this is irrelevant to you, if you don't have rights to manage roles yourself.`,
     ignore: true
 });
 

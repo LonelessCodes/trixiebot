9283dfcfc71493d5cb523a34de9b73b27bc139aa
Chris Shepherd <chris@loneless.org>
2018-01-14 00:42:49 +0100

    Added timeout list and clear as well as timeout fixes

--- features/admin.js
+++ features/admin.js
@@ -58,7 +58,7 @@ const command = new Command(async function onmessage(message) {
     if (await timeout.has(message.guild.id, message.member.id)) {
         const content = message.content;
         await message.delete();
-        await message.channel.send(`${message.member.toString()} You've been timeouted from writing in this server. I didn't throw your message away, you can check on it using \`!timeout my messages\`, so you can send it again when your timeout is over in __**${toHumanTime((await timeout.get(message.guild.id, message.member.id)).getTime() - Date.now())}**__`);
+        await message.channel.send(`${message.member.toString()} You've been timeouted from writing in this server. I didn't throw your message away, you can check on it using \`!timeout my messages\`, so you can send it again when your timeout is over in __**${toHumanTime((await timeout.get(message.guild.id, message.member.id)).expiresAt.getTime() - Date.now())}**__`);
         log(`Sent timeout notice to user ${message.member.user.username} in guild ${message.guild.name} and saved their message before deletion`);
         return;
     }
@@ -86,6 +86,58 @@ const command = new Command(async function onmessage(message) {
         return;
     }
 
+    if (/^\!timeout list\b/i.test(message.content)) {
+        const permission = message.channel.permissionsFor(message.member).has(Discord.Permissions.FLAGS.MANAGE_MESSAGES);
+        if (!permission) {
+            await message.channel.send("IDK what you're doing here, Mister Not-Allowed-To-List-Timeouts. To use the timeout command you must have permissions to manage messages.");
+            log("Gracefully aborted attempt to list timeouts without the required rights to do so");
+            return;
+        }
+
+        let longestName = 0;
+        let longestString = 0;
+        const timeouts = (await timeout.entries(message.guild.id)).map(timeout => {
+            timeout.member = message.guild.members.has(timeout.memberId) ?
+                message.guild.members.get(timeout.memberId) :
+                null;
+            if (longestName < timeout.member.displayName.length) {
+                longestName = timeout.member.displayName.length;
+            }
+            timeout.string = toHumanTime(timeout.expiresAt.getTime() - Date.now());
+            if (longestString < timeout.string.length) {
+                longestString = timeout.string.length;
+            }
+            return timeout;
+        }).filter(timeout => !!timeout.member);
+        let str = "```";
+        for (let timeout of timeouts) {
+            str += "\n";
+            str += timeout.member.displayName;
+            str += new Array(longestName - timeout.member.displayName.length).fill(" ").join("");
+            str += " | ";
+            str += timeout.string;
+        }
+        str += "\n```";
+        await message.channel.send(str);
+        log(`Sent list of timeouts in guild ${message.guild.name}`);
+        return;
+    }
+
+    if (/^\!timeout clear\b/i.test(message.content)) {
+        const permission = message.channel.permissionsFor(message.member).has(Discord.Permissions.FLAGS.MANAGE_MESSAGES);
+        if (!permission) {
+            await message.channel.send("IDK what you're doing here, Mister Not-Allowed-To-Timeout. To use the timeout command you must have permissions to manage messages.");
+            log("Gracefully aborted attempt to clear all timeouts without the required rights to do so");
+            return;
+        }
+
+        await timeout.delete(message.guild.id);
+
+        await message.channel.send("Removed all timeouts successfully");
+        log(`Removed all timeouts in guild ${message.guild.name}`);
+        return;
+    }
+
     if (/^\!timeout remove\b/i.test(message.content)) {
         const permission = message.channel.permissionsFor(message.member).has(Discord.Permissions.FLAGS.MANAGE_MESSAGES);
         if (!permission) {
@@ -98,7 +150,7 @@ const command = new Command(async function onmessage(message) {
 
         const promises = members.map(member => timeout.delete(member.guild.id, member.id));
 
-        await message.channel.send(`Removed timeouts for ${members.map(member => member.toString()).join(" ")} successfully`);
+        await message.channel.send(`Removed timeouts for ${members.map(member => member.displayName).join(" ")} successfully`);
 
         await Promise.all(promises);
         log(`Removed timeout from users ${members.map(member => member.user.username).join(" ")} in guild ${message.guild.name}`);
@@ -153,16 +205,16 @@ const command = new Command(async function onmessage(message) {
 
         msg = msg.trim();
 
-        const timeout = parseHumanTime(msg);
-        if (timeout < 10000 || timeout > 1000 * 3600 * 24 * 3) {
+        const ms = parseHumanTime(msg);
+        if (ms < 10000 || ms > 1000 * 3600 * 24 * 3) {
             await message.channel.send("Timeout length should be at least 10 seconds long and shorter than 3 days");
             log(`Gracefully aborted attempt to timeout for longer or shorter than allowed. Value: ${msg}`);
             return;
         }
 
-        const promises = members.map(member => timeout.set(member.guild.id, member.id, timeout));
+        const promises = members.map(member => timeout.set(member.guild.id, member.id, ms));
 
-        await message.channel.send(`Timeouted ${members.map(member => member.toString()).join(" ")} for ${msg} successfully`);
+        await message.channel.send(`Timeouted ${members.map(member => member.displayName).join(" ")} for ${msg} successfully`);
         
         await Promise.all(promises);
         log(`Timeouted users ${members.map(member => member.user.username).join(" ")} in guild ${message.guild.name} with ${msg}`);
@@ -174,7 +226,11 @@ const command = new Command(async function onmessage(message) {
 \`user mention\` - user to timeout. Multiple users possible
 
 \`!timeout remove <user mention 1> <user mention 2> ... \`
-\`user mention\` - user to remove timeout from. Multiple users possible`
+\`user mention\` - user to remove timeout from. Multiple users possible
+
+\`!timeout clear\` remove all timeouts
+
+\`!timeout list\` list all timeouts present at the moment`
 });
 
 module.exports = command;
--- modules/admin.js
+++ modules/admin.js
@@ -8,60 +8,83 @@ if (!global._admin) {
          * @type {Map<string, { job: Cron.CronJob; timeout: Date }>}
          */
         cache: new Map,
-        async set(guildId, userId, timeout) {
-            const row = await db.get(`SELECT * FROM timeouts WHERE guildId = "${guildId}" AND userId = "${userId}"`);
+        async set(guildId, memberId, ms) {
+            const row = await db.get(`SELECT * FROM timeouts WHERE guildId = "${guildId}" AND memberId = "${memberId}"`);
             if (!row) {
-                await db.run("INSERT INTO timeouts (guildId, userId, timeout) VALUES (?, ?, ?)", [guildId, userId, Date.now() + timeout]);
+                await db.run("INSERT INTO timeouts (guildId, memberId, expiresAt) VALUES (?, ?, ?)", [guildId, memberId, Date.now() + ms]);
             } else {
-                await db.run(`UPDATE timeouts SET timeout = ${Date.now() + timeout} WHERE guildId = "${guildId}" AND userId = "${userId}"`);
+                await db.run(`UPDATE timeouts SET timeout = ${Date.now() + ms} WHERE guildId = "${guildId}" AND memberId = "${memberId}"`);
             }
 
             const job = new Cron.CronJob({
-                cronTime: new Date(Date.now() + timeout),
+                cronTime: new Date(Date.now() + ms),
                 onTick() {
-                    timeout.delete(guildId, userId);
+                    timeout.delete(guildId, memberId);
                 },
                 start: true
             });
-            timeout.cache.set(`${guildId}:${userId}`, {
-                timeout: new Date(Date.now() + timeout),
+            timeout.cache.set(`${guildId}:${memberId}`, {
+                expiresAt: new Date(Date.now() + ms),
                 job
             });
         },
-        async get(guildId, userId) {
-            return timeout.cache.get(`${guildId}:${userId}`).timeout;
+        async get(guildId, memberId) {
+            return timeout.cache.get(`${guildId}:${memberId}`);
         },
-        async has(guildId, userId) {
-            return timeout.cache.has(`${guildId}:${userId}`);
+        async has(guildId, memberId) {
+            return timeout.cache.has(`${guildId}:${memberId}`);
         },
-        async delete(guildId, userId) {
-            timeout.cache.delete(`${guildId}:${userId}`);
-            await db.get(`DELETE FROM timeouts WHERE guildId = "${guildId}" AND userId = "${userId}"`);
+        async delete(guildId, memberId) {
+            if (typeof memberId === "string") {
+                timeout.cache.delete(`${guildId}:${memberId}`);
+                await db.get(`DELETE FROM timeouts WHERE guildId = "${guildId}" AND memberId = "${memberId}"`);
+            } else {
+                for (let key of timeout.cache.keys()) {
+                    if (key.split(":")[0] !== guildId) continue;
+                    timeout.cache.delete(key);
+                }
+                await db.get(`DELETE FROM timeouts WHERE guildId = "${guildId}"`);
+            }
+        },
+        async entries(guildId) {
+            const keys = Array.from(timeout.cache.keys());
+            return keys.filter(key => {
+                return key.split(":")[0] === guildId;
+            }).map(key => {
+                const split = key.split(":");
+                const result = timeout.cache.get(`${guildId}:${split[1]}`);
+                return {
+                    guildId,
+                    memberId: split[1],
+                    expiresAt: result.expiresAt,
+                    job: result.job
+                };
+            });
         },
         async init() {
             if (timeout.initialized) return;
             timeout.initialized = true;
 
-            await db.run("CREATE TABLE IF NOT EXISTS timeouts (guildId TEXT, userId TEXT, timeout INTEGER)");
+            await db.run("CREATE TABLE IF NOT EXISTS timeouts (guildId TEXT, memberId TEXT, expiresAt INTEGER)");
             const rows = await db.all("SELECT * FROM timeouts");
 
             for (const row of rows) {
-                const timeout = new Date(row.timeout);
+                const expiresAt = new Date(row.timeout);
                 const job = new Cron.CronJob({
-                    cronTime: timeout,
+                    cronTime: expiresAt,
                     onTick() {
-                        timeout.delete(row.guildId, row.userId);
+                        timeout.delete(row.guildId, row.memberId);
                     },
                     start: true
                 });
 
                 if (!job.running) {
-                    timeout.delete(row.guildId, row.userId);
+                    timeout.delete(row.guildId, row.memberId);
                 }
                 else {
                     // is in db, only add to cache
-                    timeout.cache.set(`${row.guildId}:${row.userId}`, {
-                        timeout,
+                    timeout.cache.set(`${row.guildId}:${row.memberId}`, {
+                        expiresAt,
                         job
                     });
                 }
@@ -77,10 +100,11 @@ if (!global._admin) {
 /**
  * @type 
  {{ timeout: { 
-   set(guildId: string, memberId: string, timeout: number): Promise<void>;
-   get(guildId: string, memberId: string): Promise<number>;
+   set(guildId: string, memberId: string, ms: number): Promise<void>;
+   get(guildId: string, memberId: string): Promise<Date>;
    has(guildId: string, memberId: string): Promise<boolean>;
-   delete(guildId: string, memberId: string): Promise<void>;
+   delete(guildId: string, memberId?: string): Promise<void>;
+   entries(guildId: string): Promise<Array<{ guildId: string; memberId: string; expiresAt: Date; job: Cron.CronJob }>>;
    init(): Promise<void>;
  }; }}
  */

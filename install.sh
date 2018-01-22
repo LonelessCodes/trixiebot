17380e815459fe844848125dfd25aa75a15a2a79
Chris Shepherd <chris@loneless.org>
2018-01-22 15:53:15 +0100

    Added automatic install all script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+apt-get update
+
+curl -sL https://deb.nodesource.com/setup_9.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt-get update
+apt-get install nodejs sqlite3 ffmpeg
+rm nodesource_setup.sh
+
+npm install -g pm2
+npm install

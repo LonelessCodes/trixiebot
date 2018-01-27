a29bd26af4255e2153a2af26251c02bb41a6a38c
Chris Shepherd <chris@loneless.org>
2018-01-27 14:12:39 +0100

    Moved complicated website stuff over to 'website' and replaced with placeholder in 'master'

--- src/web/index.js
+++ src/web/index.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const log = require("../modules/log");
 const path = require("path");
-const btoa = require("btoa");
-const fetch = require("node-fetch");
 const statistics = require("../logic/statistics");
 const http = require("http");
 const https = require("https");
@@ -13,23 +11,10 @@ const compression = require("compression");
 const serveStatic = require("serve-static");
 const socketIO = require("socket.io");
 const MongoStore = require("connect-mongo")(session);
-const Dashboard = require("./Dashboard");
 
 const privateKey = fs.readFileSync("/etc/letsencrypt/live/trixie.loneless.org-0003/privkey.pem", "utf8");
 const certificate = fs.readFileSync("/etc/letsencrypt/live/trixie.loneless.org-0003/cert.pem", "utf8");
 const credentials = { key: privateKey, cert: certificate };
-
-function options(addobj) {
-    const now = new Date;
-    const obj = {
-        description: "Discord Bot to query Derpibooru.org and much much more!",
-        copyright: now.getFullYear() === 2018 ? "2018" : "2018 - " + now.getFullYear()
-    };
-    for (let key in addobj) {
-        obj[key] = addobj[key];
-    }
-    return obj;
-}
 
 class WebApp {
     constructor(client, config, db) {
@@ -75,7 +60,6 @@ class WebApp {
     }
 
     addMiddlewares() {
-        this.app.set("view engine", "ejs");
         this.app.set("trust proxy", 1);
 
         this.app.use(helmet());
@@ -97,46 +81,11 @@ class WebApp {
     addRoutes() {
         this.app.get("/", (req, res) => {
             res.status(200).sendFile(path.join(__dirname, "static", "index.html"));
-            res.render("pages/index", options({
-                log: []
-            }));
         });
-
-        this.app.get("/login", (req, res) => {
-            res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${this.auth.client_id}&scope=identify%20guilds&response_type=code&redirect_uri=${this.redirect}`);
-        });
-
-        this.app.get("/logout", async (req, res) => {
-            req.session.destroy(() => {
-                res.redirect("/");
-            });
-        });
-
-        this.app.get("/callback", async (req, res) => {
-            if (!req.query.code) throw new Error("NoCodeProvided");
-            const code = req.query.code;
-            const creds = btoa(`${this.auth.client_id}:${this.auth.client_secret}`);
-
-            const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${this.redirect}`, {
-                method: "POST",
-                headers: {
-                    "Authorization": `Basic ${creds}`,
-                }
-            });
-            const json = await response.json();
-
-            req.session.authenticated = true;
-            req.session.access_token = json.access_token;
-            req.session.refresh_token = json.refresh_token;
-
-            res.redirect("/dashboard");
-        });
-
-        this.app.use("/dashboard", new Dashboard(this, this.client, this.config, this.db).router);
 
         this.app.all("*", (req, res) => {
             res.status(404);
-            if (req.method === "GET") res.render("pages/404");
+            if (req.method === "GET") res.sendFile(path.join(__dirname, "static", "404.html"));
             else res.end();
         });
     }

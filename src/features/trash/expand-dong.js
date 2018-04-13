d59e72cb09e6854b572d1dc0ec143c24e5c91e42
Chris Shepherd <chris@loneless.org>
2018-04-13 18:00:20 +0200

    Expand dong!!!

---
+++ src/features/trash/expand-dong.js
@@ -0,0 +1,19 @@
+const { timeout } = require("../../modules/util");
+const log = require("../../modules/log");
+const Command = require("../../class/Command");
+
+class ExpandCommand extends Command{
+    async onmessage(message) {
+        if (/^!expand dong\b/i.test(message.content)) {
+            let progress = 3;
+            const dick = await message.channel.send(`8${new Array(progress).fill("=").join("")}D`);
+            while (progress++ < 30) {
+                await timeout(1000); // can't go faster because of rate limits
+                await dick.edit(`8${new Array(progress).fill("=").join("")}D`);
+            }
+            log("Requested dong");
+        }
+    }
+}
+
+module.exports = ExpandCommand;

e21a3b116685977a919ec0266bc68cfefd12785c
Chris Shepherd <chris@loneless.org>
2018-05-04 15:17:18 +0200

    locale.js update

--- src/logic/locale.js
+++ src/logic/locale.js
@@ -1,4 +1,3 @@
-const log = require("../modules/log");
 const fs = require("fs-extra");
 const path = require("path");
 const Gettext = require("node-gettext");
@@ -6,18 +5,56 @@ const { po } = require("gettext-parser");
 
 const gt = new Gettext;
 
-gt.on("error", error => log.error("oh nose", error));
-
-const translationsDir = path.join(__dirname, "../../resources/locale");
-const files = fs.readdirSync(translationsDir);
+const translationsDir = "../../resources/locale";
+const locales = ["en", "de", "hu"];
 const domain = "messages";
 
-files.forEach(file => {
-    const translationsFilePath = path.join(translationsDir, file);
-    const translationsContent = fs.readFileSync(translationsFilePath);
+locales.forEach(locale => {
+    const filename = `${locale}.po`;
+    const translationsFilePath = path.join(translationsDir, filename);
+    const translationsContent = fs.readSync(translationsFilePath);
 
     const parsedTranslations = po.parse(translationsContent);
-    gt.addTranslations(file.slice("TrixieBot_".length, -path.extname(file)), domain, parsedTranslations);
+    gt.addTranslations(locale, domain, parsedTranslations);
 });
 
-module.exports = gt;
+gt.setTextDomain(domain);
+
+class Cursor {
+    constructor(opts = {}) {
+        this.opts = opts;
+    }
+
+    translate(message) {
+        this.opts.translate = message;
+        return this;
+    }
+
+    ifPlural(message) {
+        this.opts.plural = message;
+        return this;
+    }
+
+    fetch(num) {
+        if (num && this.opts.plural) {
+            return gt.ngettext(this.opts.translate, this.opts.plural, num);
+        } else {
+            return gt.gettext(this.opts.translate);
+        }
+    }
+}
+
+module.exports.translate = function translate(message) {
+    return new Cursor({ translate: message });
+};
+
+module.exports.setLocale = gt.setLocale;
+
+module.exports.sendTranslated = async function (message, embed) {
+    return await this.channel.send(module.exports.autoTranslate.bind(this)(message), embed);
+};
+
+module.exports.autoTranslate = function (message) {
+    module.exports.setLocale(this.config.locale);
+    return module.exports.translate(message).fetch();
+};

11a99be682dc02522b97809719935dd4ced9fa6c
Chris Shepherd <chris@loneless.org>
2019-01-22 15:37:43 +0100

    style: added descriptive comments to each category

--- src/logic/commands/Category.js
+++ src/logic/commands/Category.js
@@ -12,19 +12,41 @@ class Category {
 }
 
 module.exports = Object.freeze({
+    // messages analysis 
     ANALYSIS: new Category(CommandPermission.USER, "Analysis"),
+
+    // actions with other users
     ACTION: new Category(CommandPermission.USER, "Action"),
+
+    // text transformation and translation
     TEXT: new Category(CommandPermission.USER, "Text"),
+
+    // stuff with audio
     AUDIO: new Category(CommandPermission.USER, "Audio"),
+
+    // currency system and stuff
     CURRENCY: new Category(CommandPermission.USER, "Currency"),
+
+    // games you can play with trixie
     GAMES: new Category(CommandPermission.USER, "Games"),
+
+    // image generation and look up
     IMAGE: new Category(CommandPermission.USER, "Image"),
     FUN: new Category(CommandPermission.USER, "Fun"),
-    MODERATION: new Category(CommandPermission.ADMIN, "Moderation"),
-    OWNER: new Category(CommandPermission.OWNER, "Owner"),
+
+    // info commands about the bot or the server
     INFO: new Category(CommandPermission.USER, "Info"),
+
+    
     UTILS: new Category(CommandPermission.USER, "Utility"),
     MISC: new Category(CommandPermission.USER, "Misc"),
 
+    // any mlp related commands
     MLP: new Category(CommandPermission.USER, "My Little Pony"),
+
+    // server moderation and administration
+    MODERATION: new Category(CommandPermission.ADMIN, "Moderation"),
+
+    // owner only commands
+    OWNER: new Category(CommandPermission.OWNER, "Owner"),
 });

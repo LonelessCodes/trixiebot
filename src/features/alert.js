085495f699f62f7fa8215225fe1851b9765619e7
Chris Shepherd <chris@loneless.org>
2019-04-16 16:06:27 +0200

    feat(alert command): added Smashcast support

--- src/features/alert.js
+++ src/features/alert.js
@@ -76,7 +76,7 @@ class Picarto extends StreamProcessor {
     async getChannel(channel, url) {
         const regexp = /^(?:http\:\/\/|https\:\/\/)?(?:www\.picarto\.tv|picarto\.tv)\/([-a-zA-Z0-9@:%_+.~]{2,25})\b/;
 
-        const [, channel_name] = regexp.exec(url);
+        const [, channel_name] = regexp.exec(url) || [];
 
         if (!channel_name) return new Config(this, channel);
 
@@ -182,7 +182,7 @@ class Twitch extends StreamProcessor {
     async getChannel(channel, url) {
         const regexp = /^(?:http\:\/\/|https\:\/\/)?(?:www\.twitch\.tv|twitch\.tv)\/([-a-zA-Z0-9@:%_+.~]{2,15})\b/;
 
-        const [, channel_name] = regexp.exec(url);
+        const [, channel_name] = regexp.exec(url) || [];
 
         if (!channel_name) return new Config(this, channel);
 
@@ -205,6 +205,7 @@ class Twitch extends StreamProcessor {
 
     async checkChanges() {
         const channels = await this.manager.getConfigs(this).toArray();
+        if (channels.length === 0) return;
 
         const set = new Set(channels.map(c => c.userId));
 
@@ -262,7 +263,7 @@ class Piczel extends StreamProcessor {
     async getChannel(channel, url) {
         const regexp = /^(?:http\:\/\/|https\:\/\/)?(?:www\.piczel\.tv|piczel\.tv)\/watch\/([-a-zA-Z0-9@:%_+.~]{2,25})\b/;
 
-        const [, channel_name] = regexp.exec(url);
+        const [, channel_name] = regexp.exec(url) || [];
 
         if (!channel_name) return new Config(this, channel);
 
@@ -345,6 +346,103 @@ class Piczel extends StreamProcessor {
     get base() { return "https://piczel.tv/api/"; }
     get url() { return "piczel.tv"; }
     get name() { return "piczel"; }
+}
+
+class Smashcast extends StreamProcessor {
+    constructor(manager) {
+        super(manager);
+
+        setInterval(() => this.checkChanges(), 60 * 1000);
+        this.checkChanges();
+    }
+
+    testURL(url) {
+        return /^(http\:\/\/|https\:\/\/)?(www\.smashcast\.tv|smashcast\.tv)\/[-a-zA-Z0-9@:%_+.~]{2,}\b/.test(url);
+    }
+
+    async request(api) {
+        const r = await fetch(this.base + api);
+        const json = await r.json();
+        if (json.error == true) throw new Error(json.error_msg);
+        return json;
+    }
+
+    async getChannel(channel, url) {
+        const regexp = /^(?:http\:\/\/|https\:\/\/)?(?:www\.smashcast\.tv|smashcast\.tv)\/([-a-zA-Z0-9@:%_+.~]{2,})\b/;
+
+        const [, channel_name] = regexp.exec(url) || [];
+
+        if (!channel_name) return new Config(this, channel);
+
+        let channelPage;
+        try {
+            channelPage = await this.request("user/" + channel_name);
+            if (channelPage.user_name === null) throw new Error("User does not exist");
+
+            const user_id = channelPage.user_id;
+            const name = channelPage.user_name;
+
+            const savedConfig = await this.getDBEntry(channel.guild, user_id);
+            if (savedConfig) return new Config(this, channel, name, user_id, savedConfig._id);
+
+            return new Config(this, channel, name, user_id);
+        } catch (err) {
+            return new Config(this, channel, channel_name);
+        }
+    }
+
+    formatURL(channel, fat = false) {
+        if (fat) return this.url + "/" + "**" + channel.name + "**";
+        else return "https://" + this.url + "/" + channel.name;
+    }
+
+    async checkChanges() {
+        const channels = await this.manager.getConfigs(this).toArray();
+        if (channels.length === 0) return;
+
+        const set = new Set(channels.map(c => c.name));
+
+        const online_channels = await this.request("media/live/" + Array.from(set).join(","));
+
+        const online = online_channels.livestream.filter(stream => stream.media_is_live !== "0");
+
+        for (let config of channels) await this.checkChange(config, online).catch(err => log.error(err));
+    }
+
+    async checkChange(config, online_channels) {
+        const oldChannel = this.online.find(oldChannel =>
+            config.userId === oldChannel.userId &&
+            config.channel.guild.id === oldChannel.channel.guild.id);
+
+        const stream = online_channels.find(stream => config.userId === stream.media_user_id);
+        if (!stream) {
+            // remove the channel from the recently online list                
+            if (config.messageId || oldChannel) this.emit("offline", oldChannel || config);
+        } else {
+            // if the channel was not recently online, set it online
+            if (oldChannel || config.messageId) return;
+
+            const views = await this.request("media/views/" + config.name);
+            const media_base = "https://edge.sf.hitbox.tv";
+
+            const onlineChannel = new OnlineChannel(config, {
+                totalviews: views.total_live_views ? parseInt(views.total_live_views) : 0,
+                title: stream.media_status || media_title,
+                avatar: media_base + stream.channel.user_logo,
+                followers: parseInt(stream.channel.followers),
+                thumbnail: media_base + stream.media_thumbnail_large,
+                language: stream.media_countries ? stream.media_countries[0] : null,
+                category: stream.category_name,
+                nsfw: !!stream.media_mature
+            });
+
+            this.emit("online", onlineChannel);
+        }
+    }
+
+    get base() { return "https://api.smashcast.tv/"; }
+    get url() { return "smashcast.tv"; }
+    get name() { return "smashcast"; }
 }
 
 class ChannelQueryCursor {
@@ -707,12 +805,13 @@ module.exports = async function install(cr, client, config, db) {
         Picarto,
         Twitch,
         Piczel,
+        Smashcast,
     ]);
 
     const alertCommand = cr.register("alert", new TreeCommand)
         .setHelp(new HelpContent()
-            .setDescription("Make Trixie announce streamers when they go live.\nSupported are Picarto and Twitch.")
-            .setUsage("<page url> <?channel>", "Subscribe Trixie to a Picarto channel!")
+            .setDescription("Make Trixie announce streamers when they go live.\nSupported are Picarto, Piczel, Twitch and Smashcast.")
+            .setUsage("<page url> <?channel>", "Subscribe Trixie to a streaming channel!")
             .addParameter("page url", "copy the url of the stream page and paste it in here")
             .addParameterOptional("channel", "the channel to post the alert to later. If omitted will be this channel"))
         .setCategory(Category.MODERATION);

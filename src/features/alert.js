3b4e5d131a801ae9c38c6d791c8464ba074fbe5a
Chris Shepherd <chris@loneless.org>
2019-04-16 15:12:23 +0200

    feat(alert command): added Piczel.tv support

--- src/features/alert.js
+++ src/features/alert.js
@@ -180,7 +180,7 @@ class Twitch extends StreamProcessor {
     }
 
     async getChannel(channel, url) {
-        const regexp = /^(?:http\:\/\/|https\:\/\/)?(?:www\.twitch\.tv|twitch\.tv)\/([-a-zA-Z0-9@:%_+.~]{2,})\b/;
+        const regexp = /^(?:http\:\/\/|https\:\/\/)?(?:www\.twitch\.tv|twitch\.tv)\/([-a-zA-Z0-9@:%_+.~]{2,15})\b/;
 
         const [, channel_name] = regexp.exec(url);
 
@@ -245,6 +245,106 @@ class Twitch extends StreamProcessor {
 
     get url() { return "twitch.tv"; }
     get name() { return "twitch"; }
+}
+
+class Piczel extends StreamProcessor {
+    constructor(manager) {
+        super(manager);
+
+        setInterval(() => this.checkChanges(), 60 * 1000);
+        this.checkChanges();
+    }
+
+    testURL(url) {
+        return /^(http\:\/\/|https\:\/\/)?(www\.piczel\.tv|piczel\.tv)\/[-a-zA-Z0-9@:%_+.~]{2,}\b/.test(url);
+    }
+
+    async getChannel(channel, url) {
+        const regexp = /^(?:http\:\/\/|https\:\/\/)?(?:www\.piczel\.tv|piczel\.tv)\/watch\/([-a-zA-Z0-9@:%_+.~]{2,25})\b/;
+
+        const [, channel_name] = regexp.exec(url);
+
+        if (!channel_name) return new Config(this, channel);
+
+        try {
+            let channelPage = await this.request("streams/" + channel_name);
+            if (channelPage.error || !channelPage.data[0] || !channelPage.data[0].user) return new Config(this, channel, channel_name);
+
+            channelPage = channelPage.data[0];
+
+            const user_id = channelPage.user.id.toString();
+            const name = channelPage.user.username;
+
+            const savedConfig = await this.getDBEntry(channel.guild, user_id);
+            if (savedConfig) return new Config(this, channel, name, user_id, savedConfig._id);
+
+            return new Config(this, channel, name, user_id);
+        } catch (err) {
+            return new Config(this, channel, channel_name);
+        }
+    }
+
+    formatURL(channel, fat = false) {
+        if (fat) return this.url + "/watch/" + "**" + channel.name + "**";
+        else return "https://" + this.url + "/watch/" + channel.name;
+    }
+
+    async request(api) {
+        const r = await fetch(this.base + api);
+        return await r.json();
+    }
+
+    async checkChanges() {
+        // get all online channels
+        /** @type {any} */
+        const piczelOnline = await this.request("streams/?nsfw=true&live_only=false");
+        if (piczelOnline.error || !piczelOnline[0]) throw new Error("Piczel error:", piczelOnline.error);
+
+        const stream = this.manager.getConfigs(this);
+
+        stream.addListener("data", config => this.checkChange(piczelOnline, config));
+        stream.once("end", () => { });
+        stream.once("error", err => { log(err); });
+    }
+
+    /**
+     * @param {any} piczelOnline 
+     * @param {Channel} savedConfig 
+     */
+    async checkChange(piczelOnline, savedConfig) {
+        const g_channel = savedConfig.channel;
+        const oldChannel = this.online.find(oldChannel =>
+            savedConfig.userId === oldChannel.userId &&
+            savedConfig.channel.guild.id === oldChannel.channel.guild.id);
+
+        const channelPage = piczelOnline.find(channelPage => savedConfig.userId === channelPage.user.id.toString());
+        if (!channelPage) {
+            // remove the channel from the recently online list                
+            if (savedConfig.messageId || oldChannel) this.emit("offline", oldChannel || savedConfig);
+        } else {
+            // if the channel was not recently online, set it online
+            if (oldChannel || savedConfig.messageId) return;
+
+            const onlineChannel = new OnlineChannel(savedConfig, {
+                title: channelPage.title,
+                followers: channelPage.follower_count,
+                avatar: channelPage.user.avatar ? channelPage.user.avatar.avatar.url : null,
+                nsfw: channelPage.adult,
+                tags: channelPage.tags,
+                thumbnail: g_channel.nsfw ?
+                    `https://piczel.tv/static/thumbnail/stream_${channelPage.id}.jpg?${Date.now()}` :
+                    channelPage.adult ?
+                        null :
+                        `https://piczel.tv/static/thumbnail/stream_${channelPage.id}.jpg?${Date.now()}`
+            });
+
+            this.emit("online", onlineChannel);
+        }
+    }
+
+    get base() { return "https://piczel.tv/api/"; }
+    get url() { return "piczel.tv"; }
+    get name() { return "piczel"; }
 }
 
 class ChannelQueryCursor {
@@ -563,27 +663,31 @@ class OnlineChannel extends Channel {
         if (this.nsfw) footer.push("NSFW");
         if (this.category) footer.push(`Category: ${this.category}`);
         if (this.game) footer.push(`Game: ${this.game}`);
-        if (this.tags) footer.push(`Tags: ${this.tags.join(", ")}`);
+        if (this.tags && this.tags.length > 0) footer.push(`Tags: ${this.tags.join(", ")}`);
 
         if (await this.manager.isCompact(this.channel.guild)) {
-            return new Discord.RichEmbed()
+            const embed = new Discord.RichEmbed()
                 .setColor(CONST.COLOR.PRIMARY)
                 .setURL(this.url)
                 .setAuthor(this.name, this.avatar)
-                .setTitle(this.title)
-                .setThumbnail(this.thumbnail)
-                .setFooter(footer.join(" | "));
+                .setTitle(this.title);
+            if (this.thumbnail) embed.setImage(this.thumbnail);
+            embed.setFooter(footer.join(" | "));
+
+            return embed;
         } else {
-            return new Discord.RichEmbed()
+            const embed = new Discord.RichEmbed()
                 .setColor(CONST.COLOR.PRIMARY)
                 .setURL(this.url)
                 .setAuthor(this.name)
-                .setTitle(this.title)
-                .addField("Followers", this.followers.toLocaleString("en"), true)
-                .addField("Total Viewers", this.totalviews.toLocaleString("en"), true)
-                .setThumbnail(this.avatar)
-                .setImage(this.thumbnail)
-                .setFooter(footer.join(" | "));
+                .setTitle(this.title);
+            if (this.followers != null) embed.addField("Followers", this.followers.toLocaleString("en"), true);
+            if (this.totalviews != null) embed.addField("Total Viewers", this.totalviews.toLocaleString("en"), true);
+            embed.setThumbnail(this.avatar);
+            if (this.thumbnail) embed.setImage(this.thumbnail);
+            embed.setFooter(footer.join(" | "));
+            
+            return embed;
         }
     }
 }
@@ -602,6 +706,7 @@ module.exports = async function install(cr, client, config, db) {
     const manager = await new Manager(db, client, [
         Picarto,
         Twitch,
+        Piczel,
     ]);
 
     const alertCommand = cr.register("alert", new TreeCommand)

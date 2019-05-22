4b8adb1d44e99967f7a3a87741c6297307d5fb42
Chris Shepherd <chris@loneless.org>
2019-05-22 20:51:05 +0200

    feat: updated content in !trixie command

--- src/features/app.js
+++ src/features/app.js
@@ -9,7 +9,7 @@ const Category = require("../logic/commands/Category");
 
 module.exports = async function install(cr, client) {
     cr.register("donate", new TextCommand("https://paypal.me/loneless ❤"))
-        .setHelp(new HelpContent().setDescription("**TrixieBot costs $6 a month and a lot of time to maintain.**\nIf you like this bot, please consider giving the devs a little tip ❤"))
+        .setHelp(new HelpContent().setDescription("**TrixieBot costs $12 a month and a lot of time to maintain.**\nIf you like this bot, please consider giving the devs a little tip ❤"))
         .setCategory(Category.INFO);
 
     cr.register("version", new TextCommand(`v${INFO.VERSION}`))
@@ -30,9 +30,11 @@ module.exports = async function install(cr, client) {
             .setColor(CONST.COLOR.PRIMARY)
             .setAuthor("TrixieBot", client.user.avatarURL, INFO.WEBSITE)
             .setDescription("**Trixie is an all-in-one Discord Bot for pony lovers**\n\n" +
-                "She offers a variety of great features, many of which to satisfy the needs of My Little Pony fans and server admins.\n" +
+                "She offers a variety of great features, many of which to satisfy the needs of My Little Pony fans and server admins.\n\n" +
+                "Her set of commands range from utility stuff, simple fun, imageboard commands, custom commands, soundboards, to even a full web dashboard to configure Trixie and watch the growth of your server and so much more!\n" +
                 "For a list of all commands, go `" + m.guild.config.prefix + "help`.\n\n" + 
-                "Website " + INFO.WEBSITE)
+                "Website " + INFO.WEBSITE + "\n" + 
+                "Web Dashboard " + INFO.WEBSITE + "/dashboard")
             .setFooter(`TrixieBot v${INFO.VERSION}`)
     })))
         .setHelp(new HelpContent().setDescription("First command to call."))

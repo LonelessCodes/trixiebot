acb83d47740c67ae0eff073920cf3740a71b98df
Chris Shepherd <chris@loneless.org>
2019-08-10 08:16:03 +0200

    fix(status): didn't floor random array index

--- src/modules/random/random.js
+++ src/modules/random/random.js
@@ -20,8 +20,8 @@ module.exports = function random(...args) {
     } else if (args.length === 1) {
         if (typeof args[0] === "number") {
             return args[0] <= 1 ? 0 : Math.random() * args[0];
-        } else if (args[0] instanceof Array) {
-            return args[0].length <= 1 ? args[0][0] : args[0][Math.random() * args[0].length];
+        } else if (Array.isArray(args[0])) {
+            return args[0].length <= 1 ? args[0][0] : args[0][Math.floor(Math.random() * args[0].length)];
         } else {
             throw new Error("First argument should be number or Array");
         }

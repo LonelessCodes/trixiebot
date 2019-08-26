43889a50b02895bd82cf8951ccf879d953f086cb
Chris Shepherd <chris@loneless.org>
2019-08-26 11:53:55 +0200

    fix(db): switch to new server discovery engine

--- src/modules/db/database.js
+++ src/modules/db/database.js
@@ -25,6 +25,7 @@ if (!config.has("database.db"))
 const opts = {
     autoReconnect: true,
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 };
 if (config.has("database.auth")) opts.auth = config.get("database.auth");
 

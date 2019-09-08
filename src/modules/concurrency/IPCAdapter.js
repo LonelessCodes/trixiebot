89e551baa0e47e818cb60ba34f49da6b8c4fa9c5
Chris Shepherd <chris@loneless.org>
2019-09-08 22:38:43 +0200

    cleanup(ipc): make CPC interface usable for IPC server and client sockets

---
+++ src/modules/concurrency/IPCAdapter.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2018-2019 Christian Schäfer / Loneless
+ *
+ * TrixieBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * TrixieBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const events = require("events");
+// eslint-disable-next-line no-unused-vars
+const Server = require("node-ipc/dao/socketServer.js");
+const random = require("../random/random");
+const log = require("../../log").namespace("ipc");
+
+/*
+* CPC Interface
+* @exit
+* @message => data
+* #send(data)
+*/
+
+class IPCAdapter extends events.EventEmitter {
+    /**
+     * @param {Server} server
+     */
+    constructor(server) {
+        super();
+
+        this.server = server;
+        this.server.on("message", data => this.emit("message", data));
+
+        this.sockets = [];
+
+        this.promiseStart = new Promise(resolve => {
+            this.server.on("start", () => {
+                this.server.on("connect", socket => {
+                    if (this.sockets.length > 0) {
+                        socket.end();
+                        return;
+                    }
+                    this.sockets.push(socket);
+                    resolve();
+                    log(`## connected to ${socket.id} ##`);
+                });
+
+                this.server.on("socket.disconnect", (socket, socketId) => {
+                    if (this.sockets.length !== 0) return;
+                    const i = this.sockets.findIndex(s => socketId === s.id);
+                    if (i >= 0) {
+                        this.sockets.splice(i, 1);
+                        this.emit("exit");
+                    }
+
+                    log(`## disconnected from ${socketId} ##`);
+                });
+            });
+        });
+    }
+
+    getSocket() {
+        if (this.sockets.length === 0) return null;
+        return random(this.sockets);
+    }
+
+    send(data) {
+        this.promiseStart.then(() => {
+            const socket = this.getSocket();
+            if (!socket) throw new Error("No socket connected");
+            this.server.emit(socket, "message", data);
+        });
+    }
+}
+
+module.exports = IPCAdapter;
--- src/modules/concurrency/ipc.js
+++ src/modules/concurrency/ipc.js
@@ -16,8 +16,9 @@
 
 const info = require("../../info");
 const log = require("../../log").namespace("ipc");
-const uuid = require("uuid");
 const ipc = require("node-ipc");
+const IPCAdapter = require("./IPCAdapter");
+const cpc = require("./cpc");
 
 ipc.config.silent = true;
 ipc.config.id = info.DEV ? "trixiedev" : "trixiebot";
@@ -26,77 +27,6 @@ ipc.config.logger = log.bind(log);
 
 ipc.serve();
 
-const server = ipc.server;
+module.exports = cpc(new IPCAdapter(ipc.server));
 
-server.connected = false;
-server.started = false;
-
-server.promiseStart = new Promise(resolve => {
-    server.on("start", () => {
-        server.started = true;
-
-        server.on("connect", socket => {
-            server.connected = true;
-            ipc.log(`## connected to ${socket.id} ##`, ipc.config.delay);
-        });
-
-        server.on("socket.disconnect", (socket, socketId) => {
-            if (server.sockets.length !== 0) return;
-
-            server.connected = false;
-            ipc.log(`## disconnected from ${socketId} ##`);
-        });
-
-        resolve();
-    });
-});
-
-server.start();
-
-function getSocket() {
-    if (server.sockets.length === 0) return null;
-    return server.sockets[server.sockets.length - 1]; // Getting the newest connected socket
-}
-
-const oldEmit = server.emit.bind(server);
-function emit(message_bus, ...data) {
-    oldEmit(getSocket(), message_bus, ...data);
-}
-server.emit = emit.bind(server);
-
-server.answer = function answer(message_bus, handler) {
-    server.on(message_bus, async (message, socket) => {
-        const { id, data } = message;
-
-        const response = await handler(data);
-        oldEmit(socket, message_bus, {
-            id,
-            data: response,
-        });
-    });
-};
-
-server.awaitAnswer = function awaitAnswer(message_bus, data) {
-    return new Promise((resolve, reject) => {
-        const socket = getSocket();
-        if (!socket) return reject(new Error("Not connected to any socket"));
-
-        const id = uuid.v1();
-
-        const handler = (message, sock) => {
-            if (message.id !== id) return;
-            if (sock.id !== socket.id) return;
-
-            server.off(message_bus, handler);
-            resolve(message.data);
-        };
-        server.on(message_bus, handler);
-
-        oldEmit(socket, message_bus, {
-            id,
-            data,
-        });
-    });
-};
-
-module.exports = server;
+ipc.server.start();

a083b91bc15d876dad33b1f1b91de3d3be61ed0e
Chris Shepherd <chris@loneless.org>
2019-12-13 14:46:31 +0100

    breaking: remove dolan, pirate and yoda commands

--- src/commands/translation_commands.js
+++ src/commands/translation_commands.js
@@ -14,17 +14,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const fetch = require("node-fetch");
-
 const SimpleCommand = require("../core/commands/SimpleCommand");
 const OverloadCommand = require("../core/commands/OverloadCommand");
 const HelpContent = require("../util/commands/HelpContent");
 const Category = require("../util/commands/Category");
 const CommandScope = require("../util/commands/CommandScope");
-const RateLimiter = require("../util/commands/RateLimiter");
-const TimeUnit = require("../modules/TimeUnit");
-
-const baseURL = "https://api.funtranslations.com/translate/";
 
 /*
  * OWO Translator:
@@ -92,37 +86,7 @@ function translateOwo(input) {
     );
 }
 
-/*
- * Translator APIs
- */
-
-async function translate(type, text) {
-    const url = `${baseURL}${type}.json?text=${encodeURIComponent(text)}`;
-    const request = await fetch(url);
-    const { success, contents } = await request.json();
-    if (!success) {
-        return "Request failed for some reason :c";
-    }
-
-    return contents.translated;
-}
-
-function translator(type, description) {
-    return new OverloadCommand()
-        .registerOverload("1+", new SimpleCommand(({ content }) => translate(type, content)))
-        .setHelp(new HelpContent()
-            .setUsage("<text>", description)
-            .addParameter("text", "The text to translate"))
-        .setCategory(Category.FUN)
-        .setRateLimiter(new RateLimiter(TimeUnit.HOUR, 1, 2))
-        .setScope(CommandScope.ALL);
-}
-
 module.exports = function install(cr) {
-    cr.registerCommand("pirate", translator("pirate", "Translate something into pirate-ish"));
-    cr.registerCommand("yoda", translator("yoda", "Translate something into yoda-ish"));
-    cr.registerCommand("dolan", translator("dolan", "Translate something into dolan duck-ish"));
-
     cr.registerCommand("owo", new OverloadCommand)
         .registerOverload("1+", new SimpleCommand(({ content }) => translateOwo(content)))
         .setHelp(new HelpContent()

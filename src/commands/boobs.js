33a1cd0c63c4e6238ac60b1525f31d989d1e2915
Chris Shepherd <chris@loneless.org>
2020-04-19 00:14:07 +0200

    feat(boobs cmd): Deprecate boobs command

--- src/commands/boobs.js
+++ src/commands/boobs.js
@@ -14,41 +14,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const fetch = require("node-fetch");
-const CONST = require("../const");
-const Discord = require("discord.js");
-
-const SimpleCommand = require("../core/commands/SimpleCommand");
-const HelpContent = require("../util/commands/HelpContent");
+const DeprecationCommand = require("../core/commands/DeprecationCommand");
 const Category = require("../util/commands/Category");
-const CommandScope = require("../util/commands/CommandScope");
-
-async function randomBoob(reconnectTries = 0) {
-    let file;
-    try {
-        const response = await fetch("http://api.oboobs.ru/boobs/0/1/random");
-        const result = await response.json();
-        file = "http://media.oboobs.ru/" + result[0].preview.replace("_preview", "");
-    } catch (err) {
-        reconnectTries++;
-        if (reconnectTries > 5) throw err;
-        file = await randomBoob(reconnectTries);
-    }
-
-    return file;
-}
 
 module.exports = function install(cr) {
-    cr.registerCommand("boobs", new SimpleCommand(async () => {
-        const url = await randomBoob();
-        return new Discord.RichEmbed()
-            .setColor(CONST.COLOR.PRIMARY)
-            .setImage(url);
-    }))
-        .setHelp(new HelpContent().setUsage("", "I wonder what this does"))
-        .setExplicit(true)
-        .setCategory(Category.IMAGE)
-        .setScope(CommandScope.ALL);
+    cr.registerCommand("boobs", new DeprecationCommand("The boobs command is no longer part of TrixieBot."))
+        .setCategory(Category.IMAGE);
     cr.registerAlias("boobs", "boobies");
     cr.registerAlias("boobs", "bewbs");
 };

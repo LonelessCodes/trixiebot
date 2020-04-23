5f09baff94a8669f61ce6cb0ecf36e5b2a65c9ba
Chris Shepherd <chris@loneless.org>
2020-04-23 22:48:05 +0200

    chore(ts): migrate getNextSequence.js to getNextSequence.ts

--- src/modules/db/getNextSequence.ts
+++ src/modules/db/getNextSequence.ts
@@ -3,36 +3,31 @@
  * Created by Alexey Chistyakov <ross@newmail.ru> on 11.10.2014.
  */
 
-const { tick } = require("../../util/promises");
+import { tick } from "../../util/promises";
+import { Db } from "mongodb";
 
 /**
  * Get next sequence id for the given collection in the given database
  *
- * @param {MongodbNativeDriver} db Connection to mongodb native driver
- * @param {any} context Current context to get auto increment field for
+ * @param {Db} db Connection to mongodb native driver
+ * @param {string} context Current context to get auto increment field for
  * @returns {Promise<number>}
  */
-async function getNextSequence(db, context) {
+async function getNextSequence(db: Db, context: string): Promise<number> {
     const collection = db.collection("inc_counters");
 
     try {
-        const result = await collection.findOneAndUpdate(
-            { _id: context },
-            { $inc: { seq: 1 } },
-            { upsert: true }
-        );
+        const result = await collection.findOneAndUpdate({ _id: context }, { $inc: { seq: 1 } }, { upsert: true });
 
         if (result.value && typeof result.value.seq === "number") {
             return result.value.seq;
-        } else {
-            return result.seq;
         }
+        return (result as any).seq as number;
     } catch (err) {
         if (err.code == 11000) {
             return await tick().then(() => getNextSequence(db, context));
-        } else {
-            throw err;
         }
+        throw err;
     }
 }
-module.exports = getNextSequence;
+export default getNextSequence;

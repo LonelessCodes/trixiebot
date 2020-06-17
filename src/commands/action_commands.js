93c642799e49c97d693996dcce1d3f05a3a4cfcb
Loneless <gitkraken@loneless.art>
2020-06-17 19:50:35 +0200

    deprecation: remove less used commands: pat, touch, point, poke, bite, lick, tickle, smile, stare, holdhands, cuddle, snuggle, nuzzle, nom, ascii, fuck

--- src/commands/action_commands.js
+++ src/commands/action_commands.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2019 Christian Schäfer / Loneless
+ * Copyright (C) 2018-2020 Christian Schäfer / Loneless
  *
  * TrixieBot is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,9 +37,6 @@ module.exports = function install(cr) {
     )).setAllowEveryone(true);
     cr.registerAlias("hug", "hugs");
 
-    cr.registerCommand("pat", new TextActionCommand("*patpat*", "*patpats {{user}}*", new Translation("action.pat.self", "Aww, take a pat <3")))
-        .setAllowEveryone(true);
-
     cr.registerCommand("kiss", new TextActionCommand("Kiss someone -3-", [
         "( ˘ ³˘){{user}}",
         "（*＾3＾）{{user}}",
@@ -52,69 +49,6 @@ module.exports = function install(cr) {
     ], new Translation("action.kiss.self", "Aww, *kisses*")))
         .setAllowEveryone(true);
 
-    cr.registerCommand("touch", new TextActionCommand("Touch someone o.o\"", [
-        "{{user}}ԅ(‾⌣‾ԅ)",
-        "{{user}}ԅ╏ ˵ ⊚ ◡ ⊚ ˵ ╏┐",
-        "(¬_¬”)-cԅ(‾⌣‾ԅ)",
-        "{{user}}ԅ( ˘ω˘ ԅ)",
-        "{{user}}ԅ(≖‿≖ ;ԅ)",
-        "{{user}}ԅ(‹o›Д‹o›ԅ)",
-    ], "{{user}}ԅ(‾⌣‾ԅ)"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("point", new TextActionCommand("Point at someone", [
-        "(☞ﾟ∀ﾟ)☞ {{user}}",
-        "( ´థ౪థ)σ’`ﾞ {{user}}",
-        "┗(•ˇ_ˇ•)―→ {{user}}",
-        "〈(•ˇ‿ˇ•)-→ {{user}}",
-        "（☞´^ิ∀^ิ｀）☞ {{user}}",
-        "(╭☞´ิ∀´ิ)╭☞ {{user}}",
-        "(╭☞•́⍛•̀)╭☞ {{user}}",
-        "(☞^o^) ☞ {{user}}",
-        "☞๏็ັཪ๏็๎☞ {{user}}",
-        "( ━☞´◔‿ゝ◔`)━☞ {{user}}",
-        "(☞三☞ ఠ ਉ ఠ))☞三☞ {{user}}",
-    ], new Translation("action.point.self", ">:3 *points at u*")))
-        .setAllowEveryone(true);
-
-    // eslint-disable-next-line no-warning-comments
-    // TODO: improve these down here
-
-    cr.registerCommand("poke", new TextActionCommand("Poke poke", "*pokes {{user}}*", "Aww, *pokes you* :eyes:"))
-        .setAllowEveryone(true);
-
     cr.registerCommand("slap", new TextActionCommand("Slap someone real dirty >:c", "*slaps {{user}}*", "Hmm, why do you want this? Uh, I guess... *slaps you*"))
         .setAllowEveryone(true);
-
-    cr.registerCommand("bite", new TextActionCommand("Ouchie! Bite someone", "*bites {{user}}* :eyes:", "*bites you*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("lick", new TextActionCommand("Lick someone clean!", "*licks {{user}}* :heart:", "*licks you*"))
-        .setAllowEveryone(true)
-        .setExplicit(true);
-
-    cr.registerCommand("tickle", new TextActionCommand("Nuuuu I'm so ticklish!!!! >~<\nTickle someone", "*tickles {{user}}*", "*tickles you*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("smile", new TextActionCommand("Smile at someone", "*smiles at {{user}}* :heart:", "*smiles at you*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("stare", new TextActionCommand("Stare at someone :eyes:", "*stares at {{user}}* :eyes:", "*stares at you*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("holdhands", new TextActionCommand("Hold hands with a cutie", "*holding {{user}}'s hand* :heart:", "*holding your hand*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("cuddle", new TextActionCommand("Cuddle someone and give them a warm feeling", "*cuddles {{user}}* :heart:", "*cuddles you*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("snuggle", new TextActionCommand("Snuggle someone and die from the cuteness!!! >3<", "*snuggles {{user}}* :heart:", "*snuggles you*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("nuzzle", new TextActionCommand("Nuzzle someone", "*nuzzles {{user}}*", "*nuzzles you*"))
-        .setAllowEveryone(true);
-
-    cr.registerCommand("nom", new TextActionCommand("nomnom someone", "*nomnoms {{user}}*", "*nomnoms you*"))
-        .setAllowEveryone(true);
-    cr.registerAlias("nom", "nomnom");
 };
